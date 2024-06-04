339768b6f6b4435b85428a2626e1ee3b6326bed3
Eskie Maquilang <eskiesiriusmaquilang@gmail.com>
2024-06-04 15:17:30 +0800

    invoice

--- routes/web.php
+++ routes/web.php
@@ -1,1 +1,7 @@
 <?php
+
+use App\Http\Controllers\SaleController;
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/sales/{sale}/invoice', [SaleController::class, 'generateInvoice'])->name('sales.generate-invoice');
+});
