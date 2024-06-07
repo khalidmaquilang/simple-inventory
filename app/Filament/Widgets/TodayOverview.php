5d7674b2e67b8eb8259bb5dcb6257b6be793a6f1
Eskie Maquilang <eskiesiriusmaquilang@gmail.com>
2024-06-08 04:40:27 +0800

    additional widget

---
+++ app/Filament/Widgets/TodayOverview.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Setting;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+use Illuminate\Support\Facades\DB;
+
+class TodayOverview extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        $range = range(1, 5);
+        shuffle($range);
+
+        return [
+            Stat::make("Today's Total Purchases Due", $this->getTotalPurchasesDue())
+                ->color('warning')
+                ->chart($range),
+            Stat::make("Today's Total Purchases Amount", $this->getTotalPurchasesAmount())
+                ->color('success')
+                ->chart($range),
+            Stat::make("Today's Total Sales Due", $this->getTotalSalesDue())
+                ->color('warning')
+                ->chart($range),
+            Stat::make("Today's Total Sales Amount", $this->getTotalSalesAmount())
+                ->color('success')
+                ->chart($range),
+        ];
+    }
+
+    /**
+     * @return string
+     */
+    protected function getTotalPurchasesDue(): string
+    {
+        return $this->formatCurrency($this->getTotalDue('purchase_orders'));
+    }
+
+    /**
+     * @return string
+     */
+    protected function getTotalPurchasesAmount(): string
+    {
+        return $this->formatCurrency($this->getTotalAmount('purchase_orders'));
+    }
+
+    /**
+     * @return string
+     */
+    protected function getTotalSalesDue(): string
+    {
+        return $this->formatCurrency($this->getTotalDue('sales'));
+    }
+
+    /**
+     * @return string
+     */
+    protected function getTotalSalesAmount(): string
+    {
+        return $this->formatCurrency($this->getTotalAmount('sales'));
+    }
+
+    /**
+     * @param  $tableName
+     * @return string
+     */
+    protected function getTotalDue($tableName): string
+    {
+        $today = now()->toDateString();
+        $column = $tableName === 'sales' ? 'sale_date' : 'order_date';
+
+        return cache()->remember('widget-today-total-due-'.$tableName, 60 * 3, function () use ($tableName, $column, $today) {
+            return DB::table($tableName)
+                ->selectRaw('SUM(total_amount - paid_amount) as total_due')
+                ->whereDate($column, $today)
+                ->value('total_due');
+        });
+    }
+
+    /**
+     * @param  $tableName
+     * @return string
+     */
+    protected function getTotalAmount($tableName): string
+    {
+        $today = now()->toDateString();
+        $column = $tableName === 'sales' ? 'sale_date' : 'order_date';
+
+        return cache()->remember('widget-today-total-amount-'.$tableName, 60 * 3, function () use ($tableName, $today, $column) {
+            return DB::table($tableName)
+                ->selectRaw('SUM(total_amount) as total_amount')
+                ->whereDate($column, $today)
+                ->value('total_amount');
+        });
+    }
+
+    /**
+     * @param  float  $amount
+     * @return string
+     */
+    protected function formatCurrency(float $amount): string
+    {
+        return number_format($amount, 2).' '.Setting::getCurrency();
+    }
+}
---
+++ app/Filament/Widgets/TotalOverview.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Setting;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+use Illuminate\Support\Facades\DB;
+
+class TotalOverview extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        $range = range(1, 5);
+        shuffle($range);
+
+        return [
+            Stat::make('Total Purchases Due', $this->getTotalPurchasesDue())
+                ->color('warning')
+                ->chart($range),
+            Stat::make('Total Purchases Amount', $this->getTotalPurchasesAmount())
+                ->color('success')
+                ->chart($range),
+            Stat::make('Total Sales Due', $this->getTotalSalesDue())
+                ->color('warning')
+                ->chart($range),
+            Stat::make('Total Sales Amount', $this->getTotalSalesAmount())
+                ->color('success')
+                ->chart($range),
+        ];
+    }
+
+    /**
+     * @return string
+     */
+    protected function getTotalPurchasesDue(): string
+    {
+        return $this->formatCurrency($this->getTotalDue('purchase_orders'));
+    }
+
+    /**
+     * @return string
+     */
+    protected function getTotalPurchasesAmount(): string
+    {
+        return $this->formatCurrency($this->getTotalAmount('purchase_orders'));
+    }
+
+    /**
+     * @return string
+     */
+    protected function getTotalSalesDue(): string
+    {
+        return $this->formatCurrency($this->getTotalDue('sales'));
+    }
+
+    /**
+     * @return string
+     */
+    protected function getTotalSalesAmount(): string
+    {
+        return $this->formatCurrency($this->getTotalAmount('sales'));
+    }
+
+    /**
+     * @param  $tableName
+     * @return string
+     */
+    protected function getTotalDue($tableName): string
+    {
+        return cache()->remember('widget-total-due-'.$tableName, 60 * 3, function () use ($tableName) {
+            return DB::table($tableName)
+                ->selectRaw('SUM(total_amount - paid_amount) as total_due')
+                ->value('total_due');
+        });
+    }
+
+    /**
+     * @param  $tableName
+     * @return string
+     */
+    protected function getTotalAmount($tableName): string
+    {
+        return cache()->remember('widget-total-amount-'.$tableName, 60 * 3, function () use ($tableName) {
+            return DB::table($tableName)
+                ->selectRaw('SUM(total_amount) as total_amount')
+                ->value('total_amount');
+        });
+    }
+
+    /**
+     * @param  float  $amount
+     * @return string
+     */
+    protected function formatCurrency(float $amount): string
+    {
+        return number_format($amount, 2).' '.Setting::getCurrency();
+    }
+}
