26bb6b57b62e91b9943f28727844a644e58f73ce
Eskie Maquilang <eskiesiriusmaquilang@gmail.com>
2024-06-21 10:17:12 +0800

    remove unused

--- resources/views/filament/invoice/invoice.blade.php
+++ resources/views/filament/invoice/invoice.blade.php
@@ -9,9 +9,6 @@
     <link type="text/css" rel="stylesheet" href="{{ public_path('app-invoice/assets/css/bootstrap.min.css') }}">
     <link type="text/css" rel="stylesheet" href="{{ public_path('app-invoice/assets/fonts/font-awesome/css/font-awesome.min.css') }}">
 
-    <!-- Favicon icon -->
-    <link rel="shortcut icon" href="{{ public_path('app-invoice/assets/img/favicon.ico') }}" type="image/x-icon" >
-
     <!-- Google fonts -->
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
