3597da664b7c00143bb536da81dfb9f49d9dce21
Eskie Maquilang <eskiesiriusmaquilang@gmail.com>
2024-06-24 08:55:51 +0800

    fix cs

--- routes/console.php
+++ routes/console.php
@@ -2,8 +2,6 @@
 
 use App\Console\Commands\CheckExpiredSubscriptions;
 use App\Console\Commands\SendInvoiceNearExpiredSubscriptions;
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command(CheckExpiredSubscriptions::class)->dailyAt('23:00');
