2f75ab4f44d979939630fea48866f5b7b6480540
Eskie Maquilang <eskiesiriusmaquilang@gmail.com>
2024-06-24 09:59:37 +0800

    install horizon

--- app/Filament/Exports/ReportExporterTrait.php
+++ app/Filament/Exports/ReportExporterTrait.php
@@ -6,8 +6,20 @@ use Filament\Actions\Exports\Models\Export;
 
 trait ReportExporterTrait
 {
+    /**
+     * @param  Export  $export
+     * @return string
+     */
     public function getFileName(Export $export): string
     {
         return "{$this->filename}-".now()->format('Y-m-d');
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getJobQueue(): ?string
+    {
+        return 'long-running-queue';
     }
 }
