bedb8e737d0bd653c0f17ca66924dba8e9c505e2
Derick Maquilang <dxmaquilang@gmail.com>
2025-02-21 05:22:18 +0800

    Fix

--- database/factories/CustomerFactory.php
+++ database/factories/CustomerFactory.php
@@ -23,7 +23,7 @@ class CustomerFactory extends Factory
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->safeEmail(),
-            'phone' => $this->faker->phoneNumber(),
+            'phone' => $this->faker->numerify('09#########'),
             'address' => $this->faker->text(),
             'gender' => $this->faker->randomElement(['male', 'female']),
             'user_id' => User::factory(),
