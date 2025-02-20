e4f87c774c3b7dd699f63133cb5ac6a9647f4690
Derick Maquilang <dxmaquilang@gmail.com>
2025-02-21 05:05:52 +0800

    Fixed whitelines.

--- database/migrations/2025_02_18_152126_add_product_image_to_products_table.php
+++ database/migrations/2025_02_18_152126_add_product_image_to_products_table.php
@@ -9,7 +9,7 @@ class AddProductImageToProductsTable extends Migration
     public function up()
     {
         Schema::table('products', function (Blueprint $table) {
-            if (!Schema::hasColumn('products', 'product_image')) {
+            if (! Schema::hasColumn('products', 'product_image')) {
                 $table->string('product_image')->nullable();
             }
         });
@@ -24,4 +24,3 @@ class AddProductImageToProductsTable extends Migration
         });
     }
 }
-
