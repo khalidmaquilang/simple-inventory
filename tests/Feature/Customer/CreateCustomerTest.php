2dc0e877709397621a5162edc4f27d7da6556a31
Derick Maquilang <dxmaquilang@gmail.com>
2025-03-15 16:35:59 +0800

    Pint

--- tests/Feature/Customer/CreateCustomerTest.php
+++ tests/Feature/Customer/CreateCustomerTest.php
@@ -41,10 +41,7 @@ class CreateCustomerTest extends TestCase
     {
         $user = $this->login(['create_customer', 'view_any_customer']);
 
-        $customer = Customer::factory()->make([
-            'user_id' => $user->id,
-            'phone' => '09291530090',
-        ])->toArray();
+        $customer = Customer::factory()->make(['user_id' => $user->id])->toArray();
 
         Livewire::test(CustomerResource\Pages\CreateCustomer::class)
             ->fillForm($customer)
